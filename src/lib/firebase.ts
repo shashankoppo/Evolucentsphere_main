32c81cb94613d3b615891870a6addefe1fcd4ee0
shashankoppo <75025202+shashankoppo@users.noreply.github.com>
2025-06-04 16:30:30 +0530

    Remove Firebase Files and Update Dependencies

--- src/lib/firebase.ts
+++
@@ -1,9 +0,0 @@
-// This file is deprecated and will be removed
-// We're using Supabase instead of Firebase
-console.warn('firebase.ts is deprecated - using Supabase instead');
-
-export const analytics = {
-  logEvent: () => {
-    console.warn('Firebase analytics is deprecated - using custom analytics instead');
-  }
-};
